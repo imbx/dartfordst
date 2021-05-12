628bd6814e7eb8a580ac879503c4fb653b1448a9
Box <22588219+imbx@users.noreply.github.com>
2021-05-12 16:52:41 +0200

    Updated key inside of object

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -3,12 +3,14 @@ using BoxScripts;
 
 [RequireComponent(typeof (Movement))]
 public class Item : InteractBase {
-
+    public bool NoEffects = false;
     public bool CanPickup = true;
+    public bool HasItemInside = false;
     private bool isLeftAction = true;
     [SerializeField] private PrimaryController controller;
     private Movement movement;
     private TransformData startTransform;
+    [SerializeField] private GameObject Son = null;
 
     void Start()
     {
@@ -16,14 +18,19 @@ public class Item : InteractBase {
     }
 
     public override void Execute(bool isLeftAction = true) {
+        Debug.Log("Execute");
         if(isLeftAction && gameControllerObject.state != GameState.LOOKITEM){
+            if(NoEffects) {
+                OnEnd();
+                return;
+            }
             if(startTransform == null) 
             {
                 startTransform = new TransformData(transform);
                 movement.SetConfig(2f, true);
                 movement.SetParameters(new TransformData(GameController.current.Hand), startTransform);
             } else movement.Invert();
-            
+            if(HasItemInside) GetComponent<BoxCollider>().enabled = false;
             gameControllerObject.ChangeState(GameState.LOOKITEM);
         }
 
@@ -40,22 +47,28 @@ public class Item : InteractBase {
     protected override void OnEnd()
     {
         Debug.Log("Should destroy now");
+        isLeftAction = false;
         GameController.current.database.AddProgressionID(_id, true);
         gameControllerObject.ChangeState(GameState.ENDLOOKITEM);
-        Destroy(gameObject);
+        if(NoEffects || CanPickup) Destroy(gameObject);
     }
 
     public override void OnExit()
     {
-        base.OnExit();
+        // base.OnExit();
         movement.Invert();
         gameControllerObject.ChangeState(GameState.ENDLOOKITEM);
+        if(HasItemInside) {
+            if(Son) GetComponent<BoxCollider>().enabled = true;
+            else this.enabled = false;
+        }
     }
 
     private void Update()
     {
         if(movement.isAtDestination)
         {
+            if(HasItemInside && Son == null) OnExit();
             if(!isLeftAction && controller.isInput2Hold)
             {
                 transform.localEulerAngles += new Vector3(-controller.CameraAxis.y, controller.CameraAxis.x, 0);
@@ -63,8 +76,6 @@ public class Item : InteractBase {
             else if(isLeftAction)
             {
                 isLeftAction = false;
-
-                
             }
         }
 
