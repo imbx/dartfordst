c3cfb33b1405127e03b8e219643e06ee04942bc4
Roger-Montane-Soler <montanesolerroger@gmail.com>
2021-05-28 23:01:55 +0200

    menus2

--- Assets/Scripts/MainMenu_R.cs
+++ Assets/Scripts/MainMenu_R.cs
@@ -5,17 +5,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu_R : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+  
 
     public void LoadScene(string nivel)
     {
